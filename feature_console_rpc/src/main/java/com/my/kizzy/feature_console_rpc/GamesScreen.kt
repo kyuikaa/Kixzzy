ece83f74bad8274019f4e8f28bf84bbd73c7d24f
ミカーアモイ <107134739+mikaaamoe@users.noreply.github.com>
2023-11-14 16:19:04 +0700

    fix(GamesScreen.kt): remove unnecessary details and smallImg properties in RpcConfig feat(GamesScreen.kt): add support for dynamic values in RpcConfig

--- feature_console_rpc/src/main/java/com/my/kizzy/feature_console_rpc/GamesScreen.kt
+++ feature_console_rpc/src/main/java/com/my/kizzy/feature_console_rpc/GamesScreen.kt
@@ -191,11 +191,11 @@ fun GamesScreen(
                                     val string = Json.encodeToString(
                                         RpcConfig(
                                             name = info.game_title,
-                                            details = "Playing On ${info.platform}",
+                                            details = "",
                                             timestampsStart = System.currentTimeMillis().toString(),
                                             status = "dnd",
                                             largeImg = info.large_image ?: "",
-                                            smallImg = info.small_image,
+                                            smallImg = "",
                                             type = "0",
                                         )
                                     )
